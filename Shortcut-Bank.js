f346fa86ea2f2f199ed02e932ebeeb91f4210333
Shailendr Singh <ivyansh9897@gmail.com>
2015-07-08 07:26:04 +0530

    Add commands file initial stage

---
+++ Shortcut-Bank.js
@@ -0,0 +1,28 @@
+/**
+ * The Eclipse Shortcut keys.
+ *
+ */
+
+//=====================================================
+//  Manage Files and Projects
+//=====================================================
+
+Ctrl + N						Create new project using the wizard
+Ctrl + Alt + N 					Create new project, file, class, etc.
+Alt  + f, then .				Open project, file, etc.
+Ctrl + Shift + R				Open Resource(file, folder or project)
+Alt  + Enter					Show and access file properties
+Ctrl + S 						Save current Files
+Ctrl + Shift + S 				Save all Files
+Ctrl + W 						Close current file
+Ctrl + Shift + W 				Close all Files
+F5								Refresh content of selected element with local file system  
+
+//=====================================================
+//  Editor Window
+//=====================================================	 
+
+/*
+ * Focus / Cursor must be in Editor Window for these to work 
+ *
+ */
